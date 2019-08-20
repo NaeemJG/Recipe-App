5f79b18c7739b92ea264ed1b001091b76d562125
Naeem <naeemj@bhemail.com>
2019-08-20 13:39:13 -0400

    Recipes displays

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,5 @@
-export const key = 'e52265aca789a36f1f8c576d7566bd4d';
+export const key = 'e4d282e839bd8db7ab16079d4ed7773a';
 export const proxy = 'https://cors-anywhere.herokuapp.com/';
+
+// Jamilthehedgehog@gmail.com  key = e4d282e839bd8db7ab16079d4ed7773a
+// Jamil.Naeem17@gmail.com     key = e52265aca789a36f1f8c576d7566bd4d
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 // const search = new Search('pizza');
@@ -71,13 +72,16 @@ const controlRecipe = async () => {
 
     if (id) {
         // Prepare UI for changes
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
 
         // Create new recipe object
         state.recipe = new Recipe(id);
         
         try {
-            // get recipe data
+            // get recipe data and parse ingredients
             await state.recipe.getRecipe();
+            console.log(state.recipe.ingredients);
             state.recipe.parseIngredients();
 
             // Calculate servings and time
@@ -85,8 +89,9 @@ const controlRecipe = async () => {
             state.recipe.calcServings();
 
             // Render recipe
-            console.log(state.recipe);
-            
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
+
         } catch (err) {
             alert('Error processing recipe!');
         }
